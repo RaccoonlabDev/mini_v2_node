3cc7607b7e3562308cfd04df31e0e6625431c6b9
PonomarevDA <ponomarevda96@gmail.com>
2023-10-17 22:08:22 +0300

    update stm32cubemx project submodule

--- Src/cyphal_application/application.hpp
+++ Src/cyphal_application/application.hpp
@@ -1,10 +1,18 @@
 /// This software is distributed under the terms of the MIT License.
-/// Copyright (c) 2022 Dmitry Ponomarev.
+/// Copyright (c) 2022-2023 Dmitry Ponomarev.
 /// Author: Dmitry Ponomarev <ponomarevda96@gmail.com>
 
 #ifndef SRC_APPLICATION_HPP_
 #define SRC_APPLICATION_HPP_
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 void application_entry_point();
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif  // SRC_APPLICATION_HPP_
