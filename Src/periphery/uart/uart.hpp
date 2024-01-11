417007f6c08b05fd6ce7d6a1bd541a35130dcdc2
PonomarevDA <ponomarevda96@gmail.com>
2024-01-12 00:17:21 +0300

    add i2c add uart empty templates

---
+++ Src/periphery/uart/uart.hpp
@@ -0,0 +1,7 @@
+// Copyright (C) 2023 Dmitry Ponomarev <ponomarevda96@gmail.com>
+// Distributed under the terms of the GPL v3 license, available in the file LICENSE.
+
+#ifndef SRC_PERIPHERY_UART_UART_HPP_
+#define SRC_PERIPHERY_UART_UART_HPP_
+
+#endif  // SRC_PERIPHERY_UART_UART_HPP_
---
+++ Src/platform/stm32f103/uart.cpp
@@ -0,0 +1,5 @@
+// Copyright (C) 2023 Dmitry Ponomarev <ponomarevda96@gmail.com>
+// Distributed under the terms of the GPL v3 license, available in the file LICENSE.
+
+#include "periphery/uart/uart.hpp"
+#include "main.h"
