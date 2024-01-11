c005e84d154e4a93d9153407b39bfee2a9f5a419
PonomarevDA <ponomarevda96@gmail.com>
2024-01-11 19:54:41 +0300

    dronecan: use new parameters style

--- Src/dronecan_application/application.cpp
+++ Src/dronecan_application/application.cpp
@@ -5,20 +5,21 @@
 #include "dronecan.h"
 #include "main.h"
 #include "params.hpp"
-#include "string_params.hpp"
-#include "storage.h"
 #include "periphery/led/led.hpp"
 
 void application_entry_point() {
     paramsInit(static_cast<uint8_t>(IntParamsIndexes::INTEGER_PARAMS_AMOUNT), NUM_OF_STR_PARAMS);
     paramsLoadFromFlash();
 
-    auto node_id = paramsGetIntegerValue(UAVCAN_NODE_ID);
-    auto node_name = (const char*)paramsGetStringValue(static_cast<uint8_t>(IntParamsIndexes::INTEGER_PARAMS_AMOUNT));
+    auto node_id = paramsGetIntegerValue(IntParamsIndexes::PARAM_UAVCAN_NODE_ID);
+
+    const auto node_name = "co.raccoonlab.mini";
+    auto node_name_param_idx = static_cast<ParamIndex_t>(IntParamsIndexes::INTEGER_PARAMS_AMOUNT);
+    paramsSetStringValue(node_name_param_idx, 19, (const uint8_t*)node_name);
+    uavcanSetNodeName(node_name);
 
     LedPeriphery::reset();
     uavcanInitApplication(node_id);
-    uavcanSetNodeName(node_name);
 
     while(true) {
         LedPeriphery::toggle(LedColor::BLUE_COLOR);
