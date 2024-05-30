a9ab2cf24b582c560ca5c4c047a881a29dc9b345
PonomarevDA <ponomarevda96@gmail.com>
2024-05-30 22:26:01 +0300

    add module base class

---
+++ Src/common/module.cpp
@@ -0,0 +1,30 @@
+/**
+ * This program is free software under the GNU General Public License v3.
+ * See <https://www.gnu.org/licenses/> for details.
+ * Author: Dmitry Ponomarev <ponomarevda96@gmail.com>
+ */
+
+#include "module.hpp"
+
+BaseModule::BaseModule(float frequency) {
+    period_ms = (frequency > 0.001f) ? static_cast<uint32_t>(1000.0f / frequency) : 1000;
+}
+
+ModuleStatus BaseModule::get_health() const {
+    return health;
+}
+
+ModuleMode BaseModule::get_mode() const {
+    return mode;
+}
+
+void BaseModule::process() {
+    uint32_t crnt_time_ms = HAL_GetTick();
+    if (crnt_time_ms < next_spin_time_ms) {
+        return;
+    }
+
+    next_spin_time_ms = crnt_time_ms + period_ms;
+    update_params();
+    spin_once();
+}
