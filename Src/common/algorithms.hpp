b1a2339e3b43bb796daa60c6da9cb6b8853668ff
PonomarevDA <ponomarevda96@gmail.com>
2024-06-03 00:07:15 +0300

    clean up algorithms

--- Src/common/algorithms.hpp
+++ Src/common/algorithms.hpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2022-2023 Dmitry Ponomarev <ponomarevda96@gmail.com>
+ * Copyright (C) 2022-2024 Dmitry Ponomarev <ponomarevda96@gmail.com>
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at https://mozilla.org/MPL/2.0/.
@@ -17,10 +17,13 @@ extern "C" {
 typedef uint16_t PwmDurationUs;
 
 /**
- * @brief  Map raw command value (in interval from 0 to 8191)
- *         to PWM duration in us (in interval from min to max)
- * @return pwm_duration if input is correct,
- *         def_pwm if raw_command value is less than min or higher than max
+ * @brief Maps a 16-bit integer command to a PWM duration.
+ *
+ * This function takes a 16-bit integer command and maps it to a PWM duration in microseconds.
+ * If the command is outside the valid range [0, 8191], the default PWM duration is returned.
+ * Otherwise, the command is linearly mapped to the specified PWM range.
+ *
+ * @note Suitable for esc.RawCommand mapping
  */
 PwmDurationUs mapInt16CommandToPwm(int16_t rc_value,
                                    PwmDurationUs min_pwm,
@@ -28,10 +31,13 @@ PwmDurationUs mapInt16CommandToPwm(int16_t rc_value,
                                    PwmDurationUs def_pwm);
 
 /**
- * @brief  Map array command value (in interval from -1.0 to 1.0)
- *         to PWM duration in us (in interval from min to max)
- * @return pwm_duration if input is correct,
- *         def_pwm if raw_command value is less than min or higher than max
+ * @brief Maps a float command to a PWM duration.
+ *
+ * This function takes a float command and maps it to a PWM duration in microseconds.
+ * If the command is outside the valid range [-1.0, 1.0], the default PWM duration is returned.
+ * Otherwise, the command is linearly mapped to the specified PWM range.
+ *
+ * @note Suitable for actuator.Command or ratiometric setpoint mapping
  */
 PwmDurationUs mapFloatCommandToPwm(float ac_value,
                                    PwmDurationUs min_pwm,
@@ -39,14 +45,21 @@ PwmDurationUs mapFloatCommandToPwm(float ac_value,
                                    PwmDurationUs def_pwm);
 
 /**
+ * @brief Maps a value from one range to another.
+ *
+ * This function takes an input float value and linearly maps it from an input range
+ * [in_min, in_max] to an output range [out_min, out_max]. If the input value is outside
+ * the input range, the output value will be clamped to the nearest boundary of the output range.
+ */
+float mapFloat(float value,
+               float in_min, float in_max,
+               float out_min, float out_max);
+
+/**
  * @brief  Map PWM duration in us (in interval from min to max) to pct
  * @return pwm_duration in pct
  */
 float mapPwmToPct(uint16_t pwm_val, int16_t pwm_min, int16_t pwm_max);
-
-float mapFloat(float value,
-               float in_min, float in_max,
-               float out_min, float out_max);
 
 #ifdef __cplusplus
 }
