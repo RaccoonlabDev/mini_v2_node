551418ca86796e2b9f7466bfde68d2b4ccc23aef
PonomarevDA <ponomarevda96@gmail.com>
2024-06-25 22:17:05 +0300

    increase publish rate of circuit status and clean it up

--- Src/dronecan_application/modules/circuit_status/CircuitStatusModule.hpp
+++ Src/dronecan_application/modules/circuit_status/CircuitStatusModule.hpp
@@ -6,12 +6,9 @@
 #ifndef SRC_MODULES_CIRCUIT_STATUS_CIRCUIT_STATUS_HPP_
 #define SRC_MODULES_CIRCUIT_STATUS_CIRCUIT_STATUS_HPP_
 
-#include "periphery/led/led.hpp"
 #include "uavcan/equipment/temperature/Temperature.h"
-#include "periphery/adc/circuit_periphery.hpp"
 #include "uavcan/equipment/power/CircuitStatus.h"
 #include "common/module.hpp"
-#include "logger.hpp"
 
 class CircuitStatus : public Module {
 public:
@@ -24,8 +21,6 @@ protected:
 private:
     CircuitStatus_t circuit_status = {};
     Temperature_t temperature_status = {};
-
-    static Logger logger;
 
     uint8_t circuit_status_transfer_id  = 0;
     uint8_t temperature_transfer_id     = 0;
