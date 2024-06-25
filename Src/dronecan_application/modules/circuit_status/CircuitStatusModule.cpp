cdb9269fc2b2421a525eb4c007f39f9ee1c3c4ac
PonomarevDA <ponomarevda96@gmail.com>
2024-06-25 22:17:05 +0300

    increase publish rate of circuit status and clean it up

--- Src/dronecan_application/modules/circuit_status/CircuitStatusModule.cpp
+++ Src/dronecan_application/modules/circuit_status/CircuitStatusModule.cpp
@@ -4,9 +4,7 @@
 ***/ 
 
 #include "CircuitStatusModule.hpp"
-
-
-Logger CircuitStatus::logger = Logger("CircuitStatus");
+#include "periphery/adc/circuit_periphery.hpp"
 
 void CircuitStatus::init() {
     CircuitPeriphery::init();
@@ -14,35 +12,22 @@ void CircuitStatus::init() {
 }
 
 void CircuitStatus::spin_once() {
-    static uint32_t next_temp_pub_ms = 1000;
-    static uint32_t next_status_pub_ms = 1000;
+    temperature_status.temperature = CircuitPeriphery::temperature();
+    dronecan_equipment_temperature_publish(&temperature_status, &temperature_transfer_id);
+    temperature_transfer_id++;
 
+    circuit_status.voltage = CircuitPeriphery::voltage_vin();
+    circuit_status.current = CircuitPeriphery::current();
+    circuit_status.error_flags = ERROR_FLAG_CLEAR;
     if (CircuitPeriphery::voltage_5v() > 5.5 || circuit_status.voltage > 60.0) {
         circuit_status.error_flags = ERROR_FLAG_OVERVOLTAGE;
     } else if (circuit_status.current > 1.05) {
         circuit_status.error_flags = ERROR_FLAG_OVERCURRENT;
     }
-
-    if (HAL_GetTick() > next_temp_pub_ms) {
-        temperature_status.temperature = CircuitPeriphery::temperature();
-
-        dronecan_equipment_temperature_publish(&temperature_status, &temperature_transfer_id);
-        temperature_transfer_id++;
-        next_temp_pub_ms += 1000;
-    }
-
-    if (HAL_GetTick() > next_status_pub_ms) {
-        circuit_status.voltage = CircuitPeriphery::voltage_vin();
-        circuit_status.current = CircuitPeriphery::current();
-
-        dronecan_equipment_circuit_status_publish(&circuit_status, &circuit_status_transfer_id);
-        circuit_status_transfer_id++;
-        next_status_pub_ms += 1000;
-    }
+    dronecan_equipment_circuit_status_publish(&circuit_status, &circuit_status_transfer_id);
+    circuit_status_transfer_id++;
 
     if (circuit_status.error_flags != ERROR_FLAG_CLEAR) {
         health = Status::MINOR_FAILURE;
     }
-
-    circuit_status.error_flags = ERROR_FLAG_CLEAR;
 }
