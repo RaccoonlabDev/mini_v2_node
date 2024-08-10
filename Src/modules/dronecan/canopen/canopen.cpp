44d11317bf211603335386bfee9098cb1c2ce986
Dmitry Ponomarev <36133264+PonomarevDA@users.noreply.github.com>
2024-08-10 20:17:49 +0300

    add second CAN example (#54)

---
+++ Src/modules/dronecan/canopen/canopen.cpp
@@ -0,0 +1,45 @@
+/**
+ * This program is free software under the GNU General Public License v3.
+ * See <https://www.gnu.org/licenses/> for details.
+ * Author: Dmitry Ponomarev <ponomarevda96@gmail.com>
+ */
+
+#include "canopen.hpp"
+#include "can_driver.h"
+#include "common/algorithms.hpp"
+
+REGISTER_MODULE(CanopenModule)
+
+void CanopenModule::init() {
+    health = Status::OK;
+
+    if (canDriverInit(1000000, CAN_DRIVER_SECOND) < 0) {
+        health = Status::FATAL_MALFANCTION;
+    }
+
+    if (raw_command_sub.init(raw_command_cb) < 0) {
+        health = Status::FATAL_MALFANCTION;
+    }
+
+    mode = Module::Mode::OPERATIONAL;
+}
+
+void CanopenModule::spin_once() {
+    // We probably want to check TTL here
+}
+
+void CanopenModule::raw_command_cb(const RawCommand_t& msg) {
+    if (msg.size < RAW_COMMAND_CHANNEL + 1) {
+        return;
+    }
+
+    auto raw_command_value = msg.raw_cmd[RAW_COMMAND_CHANNEL];
+    uint8_t setpoint_percent = mapFloat((float)raw_command_value, 0.0f, 8191, 0.0f, 100.0f);
+
+    CanardCANFrame frame;
+    frame.id = RAW_COMMAND_CHANNEL;
+    frame.data[0] = setpoint_percent;
+    frame.data_len = 1;
+
+    canDriverTransmit(&frame, CAN_DRIVER_SECOND);
+}
---
+++ Src/modules/dronecan/canopen/canopen.hpp
@@ -0,0 +1,36 @@
+/**
+ * This program is free software under the GNU General Public License v3.
+ * See <https://www.gnu.org/licenses/> for details.
+ * Author: Dmitry Ponomarev <ponomarevda96@gmail.com>
+ */
+
+#ifndef SRC_MODULES_CANOPEN_HPP_
+#define SRC_MODULES_CANOPEN_HPP_
+
+#include "module.hpp"
+#include "subscriber.hpp"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+class CanopenModule : public Module {
+public:
+    CanopenModule() : Module(10.0) {}
+    void init() override;
+
+protected:
+    void spin_once() override;
+
+private:
+    static void raw_command_cb(const RawCommand_t& msg);
+    static inline DronecanSubscriber<RawCommand_t> raw_command_sub;
+
+    static constexpr uint8_t RAW_COMMAND_CHANNEL = 0;
+};
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif  // SRC_MODULES_CANOPEN_HPP_
