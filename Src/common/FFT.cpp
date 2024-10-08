f7a2804a691e419b3561fe3c6b2ae32ce81d8500
AsiiaPine <asiiapine@gmail.com>
2024-10-08 11:36:29 +0300

    split fft find_peaks into parts, add magnitude into RawIMU mes

--- Src/common/FFT.cpp
+++ Src/common/FFT.cpp
@@ -14,8 +14,8 @@ bool FFT::init(uint16_t window_size, uint16_t num_axes, float sample_rate_hz) {
         return false;
     }
     n_axes = num_axes;
-    rfft_spec = init_rfft(_hanning_window, _fft_input_buffer,
-                          _fft_output_buffer, &window_size);
+    rfft_spec = fft::init_rfft(_hanning_window.data(), _fft_input_buffer.data(),
+                          _fft_output_buffer.data(), &window_size);
     size = window_size;
     _sample_rate_hz = sample_rate_hz;
     _resolution_hz =  sample_rate_hz / size;
@@ -25,7 +25,7 @@ bool FFT::init(uint16_t window_size, uint16_t num_axes, float sample_rate_hz) {
 
 void FFT::update(float *input) {
     real_t conv_input[n_axes];
-    convert_float_to_real_t(input, conv_input, n_axes);
+    fft::convert_float_to_real_t(input, conv_input, n_axes);
     for (uint8_t axis = 0; axis < n_axes; axis++) {
         uint16_t &buffer_index = _fft_buffer_index[axis];
 
@@ -38,9 +38,9 @@ void FFT::update(float *input) {
             continue;
         }
 
-        apply_hanning_window(data_buffer[axis].data(), _fft_input_buffer,
-                                _hanning_window, size);
-        rfft_one_cycle(rfft_spec, _fft_input_buffer, _fft_output_buffer);
+        fft::apply_hanning_window(data_buffer[axis].data(), _fft_input_buffer.data(),
+                                _hanning_window.data(), size);
+        fft::rfft_one_cycle(rfft_spec, _fft_input_buffer.data(), _fft_output_buffer.data());
         find_peaks(axis);
 
         // reset
@@ -50,35 +50,64 @@ void FFT::update(float *input) {
                 sizeof(real_t) * overlap_start * 3);
         _fft_buffer_index[axis] = overlap_start * 3;
     }
+    find_dominant();
 }
 
 void FFT::find_peaks(uint8_t axis) {
+    float fft_output_buffer_float[size];
+    fft::convert_real_t_to_float(_fft_output_buffer.data(), fft_output_buffer_float, size);
+
     // sum total energy across all used buckets for SNR
     float bin_mag_sum = 0;
-    uint16_t num_peaks_found = 0;
-
-    float peak_magnitude[MAX_NUM_PEAKS] {};
-    uint16_t raw_peak_index[MAX_NUM_PEAKS] {};
-
-    static float peak_frequencies_prev[MAX_NUM_PEAKS] {};
-    for (int i = 0; i < MAX_NUM_PEAKS; i++) {
-        peak_frequencies_prev[i] = peak_frequencies[axis][i];
-    }
-
-    float fft_output_buffer_float[size];
-    convert_real_t_to_float(_fft_output_buffer, fft_output_buffer_float, size);
-
+    // calculate magnitudes for each fft bin
     for (uint16_t fft_index = 0; fft_index < size/2; fft_index ++) {
         real_t real_imag[2] = {0, 0};
-        get_real_imag_by_index(_fft_output_buffer, real_imag, size, fft_index);
+        fft::get_real_imag_by_index(_fft_output_buffer.data(), real_imag, size, fft_index);
         float real_f, imag_f;
-        convert_real_t_to_float(&real_imag[0], &real_f, 1);
-        convert_real_t_to_float(&real_imag[1], &imag_f, 1);
+        fft::convert_real_t_to_float(&real_imag[0], &real_f, 1);
+        fft::convert_real_t_to_float(&real_imag[1], &imag_f, 1);
         const float fft_magnitude = sqrtf(real_f * real_f + imag_f * imag_f);
         _peak_magnitudes_all[fft_index] = fft_magnitude;
         bin_mag_sum += fft_magnitude;
     }
+    float peak_magnitude[MAX_NUM_PEAKS] {};
+    uint16_t raw_peak_index[MAX_NUM_PEAKS] {};
 
+    _identify_peaks_bins(peak_magnitude, raw_peak_index);
+    auto num_peaks_found = _estimate_peaks(peak_magnitude, raw_peak_index,
+                                                fft_output_buffer_float, bin_mag_sum, axis);
+    // reset values if no peaks found
+    _fft_updated[axis] = true;
+    if (num_peaks_found == 0) {
+        for (int peak_new = 0; peak_new < MAX_NUM_PEAKS; peak_new++) {
+            peak_frequencies[axis][peak_new] = 0;
+            peak_snr[axis][peak_new] = 0;
+            peak_magnitudes[axis][peak_new] = 0;
+        }
+        return;
+    }
+}
+
+void FFT::find_dominant() {
+    if (!is_updated()) {
+        return;
+    }
+    dominant_frequency = 0;
+    dominant_snr = 0;
+    dominant_mag = 0;
+    for (uint8_t axis = 0; axis < n_axes; axis++) {
+        for (uint8_t peak = 0; peak < MAX_NUM_PEAKS; peak++) {
+            if (peak_snr[axis][peak] > dominant_snr) {
+                dominant_snr = peak_snr[axis][peak];
+                dominant_frequency = peak_frequencies[axis][peak];
+                dominant_mag = peak_magnitudes[axis][peak];
+            }
+        }
+    }
+}
+
+void FFT::_identify_peaks_bins(float peak_magnitude[MAX_NUM_PEAKS],
+                             uint16_t raw_peak_index[MAX_NUM_PEAKS]) {
     for (uint8_t i = 0; i < MAX_NUM_PEAKS; i++) {
         float largest_peak = 0;
         uint16_t largest_peak_index = 0;
@@ -104,72 +133,59 @@ void FFT::find_peaks(uint8_t axis) {
             _peak_magnitudes_all[largest_peak_index + 1] = 0;
         }
     }
+}
 
+uint16_t FFT::_estimate_peaks(float* peak_magnitude,
+                                    uint16_t* raw_peak_index,
+                                    float* fft,
+                                    float magnitudes_sum, uint8_t axis) {
+    uint16_t num_peaks_found = 0;
     for (int peak_new = 0; peak_new < MAX_NUM_PEAKS; peak_new++) {
         if (raw_peak_index[peak_new] == 0) {
             continue;
         }
         float adjusted_bin = 0.5f *
-                        estimate_peak_freq(fft_output_buffer_float, 2 * raw_peak_index[peak_new]);
+                        estimate_peak_freq(fft, 2 * raw_peak_index[peak_new]);
         if (adjusted_bin > size || adjusted_bin < 0) {
             continue;
         }
-
         float freq_adjusted = _resolution_hz * adjusted_bin;
+        // check if we already found the peak
+        bool peak_close = false;
+        for (int peak_prev = 0; peak_prev < peak_new; peak_prev++) {
+            peak_close = (fabsf(freq_adjusted - peak_frequencies[axis][peak_prev])
+                                            < (_resolution_hz * 10.0f));
+            if (peak_close) {
+                break;
+            }
+        }
+        if (peak_close) {
+            continue;
+        }
         // snr is in dB
         float snr;
-        if (bin_mag_sum - peak_magnitude[peak_new] < 1.0e-19f) {
-            if (bin_mag_sum > 0) {
+        if (magnitudes_sum - peak_magnitude[peak_new] < 1.0e-19f) {
+            if (magnitudes_sum > 0) {
                 snr = MIN_SNR;
             } else {
                 snr = 0.0f;
             }
         } else {
             snr = 10.f * log10f((size - 1) * peak_magnitude[peak_new] /
-                            (bin_mag_sum - peak_magnitude[peak_new]));
+                            (magnitudes_sum - peak_magnitude[peak_new]));
         }
+        // keep if SNR satisfies the requirement and the frequency is within the range
         if ((snr < MIN_SNR)
             || (freq_adjusted < fft_min_freq)
             || (freq_adjusted > fft_max_freq)) {
                 continue;
         }
-
-        // only keep if we're already tracking this frequency or
-        // if the SNR is significant
-        for (int peak_prev = 0; peak_prev < MAX_NUM_PEAKS; peak_prev++) {
-            bool peak_close = (fabsf(freq_adjusted - peak_frequencies_prev[peak_prev])
-                                < (_resolution_hz * 0.25f));
-            _fft_updated[axis] = true;
-
-            // keep
-            peak_frequencies[axis][num_peaks_found] = freq_adjusted;
-            peak_snr[axis][num_peaks_found] = snr;
-
-            // remove
-            if (peak_close) {
-                peak_frequencies_prev[peak_prev] = NAN;
-            }
-            num_peaks_found++;
-            break;
-        }
+        peak_frequencies[axis][num_peaks_found] = freq_adjusted;
+        peak_magnitudes[axis][num_peaks_found] = peak_magnitude[peak_new];
+        peak_snr[axis][num_peaks_found] = snr;
+        num_peaks_found++;
     }
-    if (num_peaks_found == 0) {
-        for (int peak_new = 0; peak_new < MAX_NUM_PEAKS; peak_new++) {
-            peak_frequencies[axis][peak_new] = 0;
-            peak_snr[axis][peak_new] = 0;
-        }
-        return;
-    }
-    float max_snr_peak = 0;
-    uint8_t max_peak_index = 0;
-    for (int peak_index = 0; peak_index < MAX_NUM_PEAKS; peak_index++) {
-        if (peak_snr[axis][peak_index] > max_snr_peak) {
-            max_snr_peak = peak_snr[axis][peak_index];
-            max_peak_index = peak_index;
-        }
-    }
-    peak_frequencies[axis][0] = peak_frequencies[axis][max_peak_index];
-    peak_snr[axis][0] = peak_snr[axis][max_peak_index];
+    return num_peaks_found;
 }
 
 static constexpr float tau(float x) {
@@ -190,8 +206,8 @@ float FFT::estimate_peak_freq(float fft[], int peak_index) {
     float real[3];
     float imag[3];
     for (int i = -1; i < 2; i++) {
-        real[i + 1] = get_real_by_index(fft, peak_index + i);
-        imag[i + 1] = get_imag_by_index(fft, peak_index + i);
+        real[i + 1] = fft::get_real_by_index(fft, peak_index + i);
+        imag[i + 1] = fft::get_imag_by_index(fft, peak_index + i);
     }
 
     static constexpr int k = 1;
