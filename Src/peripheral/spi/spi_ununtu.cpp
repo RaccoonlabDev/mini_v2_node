6fbd316ff469eedef597a8a49d6096bce304a2a5
Ilia <ilia112005@gmail.com>
2025-09-18 22:59:01 +0300

    fix: attempt to make SPI safer (&reg_value[-1] looked really dangerous to me)

--- Src/peripheral/spi/spi_ununtu.cpp
+++ Src/peripheral/spi/spi_ununtu.cpp
@@ -10,15 +10,15 @@
 
 namespace HAL {
 
-int8_t SPI::read_registers(std::byte reg_address, std::byte* reg_values, uint8_t size) {
+int8_t SPI::read_registers(std::byte& reg_address, std::byte* reg_values, uint8_t size) {
     (void)reg_address;
     memset(reg_values, 0x00, size);
     return -1;
 }
 
-int8_t SPI::read_register(std::byte reg_address, std::byte* reg_value) {
+int8_t SPI::read_register(std::byte& reg_address, std::byte* reg_value) {
     (void)reg_address;
-    *reg_value = 0x00;
+    *reg_value = std::byte{0x00};
     return -1;
 }
 
