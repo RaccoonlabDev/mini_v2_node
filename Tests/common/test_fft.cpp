370d4faecd682da368a379f7e021b45618103ef6
AsiiaPine <asiiapine@gmail.com>
2025-11-05 15:05:40 +0300

    fix: data scaling error in FFT, empty fft results processing in tests

--- Tests/common/test_fft.cpp
+++ Tests/common/test_fft.cpp
@@ -25,7 +25,6 @@ public:
 
     void init() {
         signals_generator.resize(n_signals);
-        uint16_t max_amplitude = 0;
 
         for (size_t j = 0; j < n_signals; j++) {
             std::uniform_int_distribution<uint16_t> freq_dist(min_freq, max_freq);
@@ -35,18 +34,19 @@ public:
             uint16_t amplitude = amp_dist(rd);
 
             signals_generator[j] = SinSignalGenerator(sample_rate_hz, freq_hz, amplitude);
-
-            if (amplitude > max_amplitude) {
-                max_amplitude = amplitude;
-                dominant_sig.emplace_back(amplitude, freq_hz);
-            }
         }
-
-        std::stable_sort(dominant_sig.begin(), dominant_sig.end(),
-            [](const std::pair<uint16_t, uint16_t>& a, const std::pair<uint16_t, uint16_t>& b) {
-                return std::less<uint16_t>{}(a.first, b.first);
-            });
+        std::stable_sort(signals_generator.begin(), signals_generator.end(),
+            [](const SinSignalGenerator& a, const SinSignalGenerator& b) {
+                return a.amplitude > b.amplitude;
+                });
+        auto domimant_size = std::min(int(n_signals), 3);
+        dominant_sig.resize(domimant_size);
+        for (size_t j = 0; j < domimant_size; j++) {
+            dominant_sig[j] = std::make_pair(signals_generator[j].amplitude,
+                                                signals_generator[j].freq_hz);
+        }
     }
+
     MultiSignalsSinGenerator() = default;
     
     MultiSignalsSinGenerator
@@ -170,6 +170,9 @@ public:
     void check_axis(int axis) {
         bool heat_peak = false;
         for (int j = 0; j < MAX_NUM_PEAKS; j++) {
+            if (fft.peak_snr[axis][j] == 0) {
+                continue;
+            }
             auto in_range = IsBetweenInclusive(fft.peak_frequencies[axis][j],
                         signals_parameters[axis].freq_hz - abs_error,
                         signals_parameters[axis].freq_hz + abs_error);
@@ -177,6 +180,12 @@ public:
                 heat_peak = true;
                 break;
             }
+        }
+        printf("peaks:\n");
+        for (int j = 0; j < MAX_NUM_PEAKS; j++) {
+            printf("peak freq: %f\n", fft.peak_frequencies[axis][j]);
+            printf("peak snr: %f\n", fft.peak_snr[axis][j]);
+            printf("peak mag: %f\n", fft.peak_magnitudes[axis][j]);
         }
         if (result) {
             EXPECT_TRUE(heat_peak);
@@ -304,7 +313,6 @@ public:
         init();
     }
 
-    
     void check_axis(int axis) {
         bool heat_peak {false};
         auto signal_generator = signals_generator[axis];
