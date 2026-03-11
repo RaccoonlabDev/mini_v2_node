e95e0e37cfc33879c35693ac77c906d186151ca8
PonomarevDA <ponomarevda96@gmail.com>
2026-03-11 23:48:12 +0300

    feat: add HAL::platform_enable_irq (required for bootloader applications)

---
+++ Src/peripheral/irq/irq_ubuntu.cpp
@@ -0,0 +1,14 @@
+/**
+ * This program is free software under the GNU General Public License v3.
+ * See <https://www.gnu.org/licenses/> for details.
+ * Author: Dmitry Ponomarev <ponomarevda96@gmail.com>
+ */
+
+#include "peripheral/irq/irq.hpp"
+
+namespace HAL {
+
+void platform_enable_irq() {
+}
+
+}  // namespace HAL
